5d44f6bf4407a480a3624c539322007bba60f9c8
silverawz <deroussenicolas@gmail.com>
2020-10-16 16:02:40 +0200

    mapping data eng/french

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,17 @@ export class AppComponent implements OnInit {
   projects:String = "Projects";
   contact:String = "Contact";
   flag_path_img:String;
+  content:String[] = new Array(" "," "," "," ");
 
   ngOnInit() {
-    this.flag_path_img = "assets/img/united-kingdom-flag-icon-32.png";
+    this.flag_path_img = "assets/img/france-flag-icon-32.png";
     this.currentSection = 1;
-    document.getElementById("mySidenav").style.width = "200px";
-    document.getElementById("main").style.marginLeft = "200px";  
+    this.content[0] = "Home page";
+    this.content[1] = "Skills page";
+    this.content[2] = "Projects page";
+    this.content[3] = "Contact page";
+    document.getElementById("mySidenav").style.width = "180px";
+    document.getElementById("main").style.marginLeft = "180px";        
   }
 
   changeSection(selected){
@@ -34,18 +39,38 @@ export class AppComponent implements OnInit {
   }
 
   changeLanguage(){
-    if(this.flag_path_img == "assets/img/united-kingdom-flag-icon-32.png"){
-      this.flag_path_img = "assets/img/france-flag-icon-32.png";
+    let language_choosed;
+    if(this.flag_path_img == "assets/img/france-flag-icon-32.png"){
+      this.flag_path_img = "assets/img/united-kingdom-flag-icon-32.png";
       this.home = "Accueil";
       this.skills = "Compétences";
       this.projects = "Projets";
-    } else{
-      this.flag_path_img = "assets/img/united-kingdom-flag-icon-32.png";
+      language_choosed = 1;
+    } else if (this.flag_path_img == "assets/img/united-kingdom-flag-icon-32.png"){
+      this.flag_path_img = "assets/img/france-flag-icon-32.png";    
       this.home = "Home";
       this.skills = "Skills";
       this.projects = "Projects";
+      language_choosed = 2;
+    }
+    this.changeContent(language_choosed);
+  }
+
+  changeContent(language_choosed){
+    if(language_choosed == 1){
+      this.content[0] = "Page d'accueil";
+      this.content[1] = "Page de compétences";
+      this.content[2] = "Page de projets";
+      this.content[3] = "Page de contact";
+    } else if (language_choosed == 2){
+      this.content[0] = "Home page";
+      this.content[1] = "Skills page";
+      this.content[2] = "Projects page";
+      this.content[3] = "Contact page";
     }
   }
+
+
   /*
   openNav() {
     console.log("aaa");
