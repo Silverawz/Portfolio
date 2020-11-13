900e21d60d18342bfac8dffcc480aac2c8c097ed
silverawz <deroussenicolas@gmail.com>
2020-11-13 02:25:44 +0100

    update navbar almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,21 @@ export class AppComponent implements OnInit {
   title = 'Portfolio';
 
   ngOnInit() {
- 
+
   }
 
-
+  section(sectionNumber){
+    if(sectionNumber != null) {
+      switch (sectionNumber) {
+        case 0: document.getElementById("effect").style.left = "12.5%";
+                break;
+        case 1: document.getElementById("effect").style.left = "37.5%";
+                break;
+        case 2: document.getElementById("effect").style.left = "62.5%";
+                break;
+        case 3: document.getElementById("effect").style.left = "87.5%";
+                break;
+      }
+    }
+  }
 }
