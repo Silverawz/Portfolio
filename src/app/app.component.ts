d7d2e629313c82ff304528fcf4bd921928209e61
silverawz <deroussenicolas@gmail.com>
2020-11-17 20:57:19 +0100

    projects done, working on contact section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,37 @@ export class AppComponent implements OnInit {
 
   title = 'Portfolio';
   projectsSelected = 1;
-
+  imgProjectUrl = "../assets/img/1.jpg";
   ngOnInit() {
-
+    
   }
 
   changeProjectText(projectNumber){
-    if(projectNumber != null)
-    this.projectsSelected = projectNumber;
+    if(projectNumber != null){
+      this.projectsSelected = projectNumber;
+      this.resetBorderColor();
+      switch (projectNumber) {
+        case 1: document.getElementById("project_1").style.borderColor = "red";
+                this.imgProjectUrl = "../assets/img/1.jpg";
+                break;
+        case 2: document.getElementById("project_2").style.borderColor = "red";
+                this.imgProjectUrl = "../assets/img/2.jpg";
+                break;
+        case 3: document.getElementById("project_3").style.borderColor = "red";
+                this.imgProjectUrl = "../assets/img/3.png";
+                break;
+        case 4: document.getElementById("project_4").style.borderColor = "red";
+                this.imgProjectUrl = "../assets/img/4.jpg";
+                break;
+      }
+    }
+  }
+
+  resetBorderColor(){
+    document.getElementById("project_1").style.borderColor = "black";
+    document.getElementById("project_2").style.borderColor = "black";
+    document.getElementById("project_3").style.borderColor = "black";
+    document.getElementById("project_4").style.borderColor = "black";
   }
 
   section(sectionNumber){
