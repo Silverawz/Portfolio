603bde72e91ca14b1dfc05af809404c3983cad8e
silverawz <deroussenicolas@gmail.com>
2021-01-08 20:10:08 +0100

    working on skills, home almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
   title = 'Portfolio';
   menu:String = "Ouvrir";
   msg_introduction:String = "Bonjour, je m'appelle Nicolas Deroussen";
-  color:String = "black";
+  color:String = "rgb(37, 189, 181)";
   timer:number = 100;
   index:number = 0;
 
@@ -24,7 +24,24 @@ export class AppComponent implements OnInit {
     this.typeWriter();
     particlesJS.load('particles-js', 'assets/data/particles.json');
     //, particlesJS.load('particles-js', 'assets/data/particles.json'function() { console.log('callback - particles.js config loaded'); });
+    window.addEventListener('scroll', this.scroll2, true); //third parameter
   }
+
+  scroll2 = (event): void => {
+    this.backAllToInitialColor();
+    if(window.scrollY >= 246 && window.scrollY < 1146){
+      document.getElementById('menu_1').style.color = "green";
+    } else if(window.scrollY >= 1146 && window.scrollY < 2388){
+      document.getElementById('menu_2').style.color = "green";
+    } else if(window.scrollY >= 2388 && window.scrollY < 4266){
+      document.getElementById('menu_3').style.color = "green";
+    }else if(window.scrollY >= 4266){
+      document.getElementById('menu_4').style.color = "green";
+    }
+  }
+
+
+
 
   typeWriter() {
     if (this.index < this.msg_introduction.length) {
@@ -38,6 +55,36 @@ export class AppComponent implements OnInit {
     }
   }
 
+  over(category){
+    document.getElementById('menu_'+category).style.color = "red";
+  }
+
+  out(category){
+    document.getElementById('menu_'+category).style.color = this.backToInitialColor();
+  }
+
+  backToInitialColor(){
+    let actualColor;
+    if(this.color == "rgb(37, 189, 181)"){
+      actualColor = "rgb(37, 189, 181)";
+    } else {
+      actualColor = "white";
+    }
+    return actualColor;
+  }
+
+  backAllToInitialColor(){
+    let actualColor;
+    if(this.color == "rgb(37, 189, 181)"){
+      actualColor = "rgb(37, 189, 181)";
+    } else {
+      actualColor = "white";
+    }
+    document.getElementById('menu_1').style.color = actualColor;
+    document.getElementById('menu_2').style.color = actualColor;
+    document.getElementById('menu_3').style.color = actualColor;
+    document.getElementById('menu_4').style.color = actualColor;
+  }
   // ngOnDestroy() {
   //   window.removeEventListener('scroll', this.scroll, true);
   // }
@@ -49,7 +96,7 @@ export class AppComponent implements OnInit {
     } 
     else if(window.scrollY == 0 && document.getElementById('nav').className == 'scroll'){
       document.getElementById('nav').className = '';
-      this.color = "black";
+      this.color = "rgb(37, 189, 181)";
     }
     
     //handle your scroll here
